93f3cfc16b577de77796f1ab36c812a00db17b8d
sefcikt <sefcikt@D017LAB-12.ac.stockton>
2017-04-24 16:08:04 -0400

    implemented input with sample

--- src/core/GA.java
+++ src/core/GA.java
@@ -1,17 +1,57 @@
 package core;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import structs.City;
+
 /**
  * @author Ryan Ceresani & Tyler Sefcik
  *
  */
 public class GA {
 
-	public static void main(String[] args) {
-		String fileName = args[0];
+	public static void main(String[] args) throws IOException {
+		//String fileName = args[0];
+		String fileName = "dj38.tsp.txt";
 		//Read file
+		ArrayList<City> cities = new ArrayList<City>();
+		cities = readCities(fileName);
 		
+		printList(cities);
 		
-
 	}
 
+	//reads cities into the cities arraylist with their number, x coordinate, and y coordinate
+	private static ArrayList<City> readCities(String fileName) throws IOException {
+		ArrayList<City> cities = new ArrayList<City>();
+		
+		String[] tokens = new String[3];
+		BufferedReader in = new BufferedReader(new FileReader(fileName));
+		String line;
+		City newCity = null;
+
+		while((line = in.readLine()) != null) {
+			if (line.contains("NAME") || line.contains("COMMENT") || line.contains("TYPE") || line.contains("DIMENSION")
+					|| line.contains("EDGE_WEIGHT_TYPE") || line.contains("NODE_COORD_SECTION") || line.contains("EOF")) {
+				continue;
+			} else {
+				tokens = line.split(" ");
+				newCity = new City(Integer.parseInt(tokens[0]), Double.parseDouble(tokens[1]), Double.parseDouble(tokens[2]));
+				if (!newCity.equals(null)) {
+					cities.add(newCity);
+				}
+			}
+		}
+		in.close();
+		return cities;
+	}
+	
+	public static void printList(ArrayList<City> list){
+	    for(City elem : list){
+	        System.out.println(elem.toString());
+	    }
+	}
 }
--- src/structs/City.java
+++ src/structs/City.java
@@ -8,14 +8,13 @@ import java.util.HashMap;
  */
 public class City {
 	private int num;
-	private long x;
-	private long y;
+	private double x;
+	private double y;
 	
 	//Remember distances to different cities
 	HashMap<Integer, Integer> memoizedDist;
 
-
-	public City(int num, long x, long y){
+	public City(int num, double x, double y){
 		this.num = num;
 		this.x = x;
 		this.y = y;
@@ -25,29 +24,33 @@ public class City {
 		return num;
 	}
 
-	public long getX() {
+	public double getX() {
 		return x;
 	}
 
-	public long getY() {
+	public double getY() {
 		return y;
 	}
 
-	public long distanceTo(City dest){
+	public int distanceTo(City dest){
 		//Check if these cities have been compared before
 		if(memoizedDist.containsKey(dest.getNum())){
 			return memoizedDist.get(dest.getNum()).intValue();
 		}
 		else{
-			long x1 = this.getX();
-			long y1 = this.getY();
-			long x2 = dest.getX();
-			long y2 = dest.getY();
+			double x1 = this.getX();
+			double y1 = this.getY();
+			double x2 = dest.getX();
+			double y2 = dest.getY();
 			
 			//Find Euclidian distance between two points
 			int distance = (int)Math.round( Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2)));
 			memoizedDist.put(dest.getNum(), distance);
 			return distance;
 		}
+	}
+	
+	public String toString(){
+		return "Num: " + num + "     X: " + x + "     Y: " + y;
 	}
 }
