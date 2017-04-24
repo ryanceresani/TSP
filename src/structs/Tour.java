ec33c65da894062b650cf5ddb81d41e7732644a1
ceresanr <ceresanr@G108LAB-26.ac.stockton>
2017-04-24 12:58:44 -0400

    Minor Tour updates

--- src/structs/Tour.java
+++ src/structs/Tour.java
@@ -5,14 +5,30 @@ import java.util.ArrayList;
 public class Tour {
 	
 	ArrayList<City> tour;
+	int fitness;
 	
 	public Tour(){
-		tour = new ArrayList<City>();
-		
+		tour = new ArrayList<City>();	
+		fitness = 0;
+	}
+	
+	public Tour(Tour cloneTour){
+		this.tour = new ArrayList<City>(cloneTour.tour);
+		fitness = cloneTour.getFitness();
+	}
+	
+	public void setCity(int index, City city){
+		tour.set(index, city);
+	}
+	
+	public City getCity(int index){
+		return tour.get(index);
 	}
 	
 	public int getFitness() {
 		// TODO Auto-generated method stub
-		return 0;
+		return fitness;
 	}
+	
+	
 }
