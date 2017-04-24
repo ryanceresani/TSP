75d565f133c5fa713df08cb5f1ef65521081d23c
ceresanr <ceresanr@D017LAB-13.ac.stockton>
2017-04-24 16:05:27 -0400

    Crossover/Tour/CityHandler
    Added NWOX crossover
    added different helper methods for Tours and Population

--- src/operators/Crossover.java
+++ src/operators/Crossover.java
@@ -1,9 +1,45 @@
 package operators;
 
+import java.util.ArrayDeque;
+import java.util.concurrent.ThreadLocalRandom;
+
+import core.GA;
+import structs.City;
+import structs.Tour;
+
 /**
  * @author Ryan Ceresani & Tyler Sefcik
  *
  */
 public class Crossover {
 
+	//Implementing Non-Wrapping Order Crossover
+	public static Tour crossover(Tour t1, Tour t2){
+		Tour child = new Tour();
+		if(ThreadLocalRandom.current().nextDouble() < GA.CROSSOVER_RATE){
+			//Do Crossover
+			int crossStart = ThreadLocalRandom.current().nextInt(t1.getSize());
+			int crossEnd = ThreadLocalRandom.current().nextInt(crossStart, t1.getSize());
+			ArrayDeque<City> holdCity = new ArrayDeque<City>();
+			Tour t2Crossover = t2.getSubTour(crossStart, crossEnd);
+
+			for (int i = 0; i < t1.getSize(); i++) {
+				if(!t2Crossover.containsCity(t1.getCity(i))){
+					holdCity.add(t1.getCity(i));
+				}
+			}
+			for (int i = 0; i < t1.getSize(); i++) {
+				if(i == crossStart){
+					child.addSubTour(t2Crossover);
+					i += t2Crossover.getSize();
+				}
+				else
+					child.setCity(i, holdCity.poll());
+			}
+		}
+		else{
+			child = new Tour(t1);
+		}
+		return child;
+	}
 }
--- src/structs/Tour.java
+++ src/structs/Tour.java
@@ -1,14 +1,28 @@
 package structs;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Tour {
 	
 	ArrayList<City> tour;
 	int fitness;
 	
+	/**
+	 * Tour Constuctors
+	 */
+	
+	/**
+	 * Create blank tour
+	 */
 	public Tour(){
-		tour = new ArrayList<City>();	
+		tour = new ArrayList<City>();
+		fitness = 0;
+	}
+	
+	public Tour(ArrayList<City> tourList){
+		tour = new ArrayList<City>(tourList);
 		fitness = 0;
 	}
 	
@@ -25,10 +39,54 @@ public class Tour {
 		return tour.get(index);
 	}
 	
-	public int getFitness() {
-		// TODO Auto-generated method stub
-		return fitness;
+	public void addSubTour(Tour subTour){
+		tour.addAll(subTour.getTour());
 	}
 	
+	public Tour getSubTour(int fromIndex, int toIndex){
+		ArrayList<City> subTourList = new ArrayList<City>(tour.subList(fromIndex, toIndex));
+		return new Tour(subTourList);
+	}
+	
+	private Collection<? extends City> getTour() {
+		return this.tour;
+	}
+
+	public boolean containsCity(City city){
+		if(tour.contains(city)){
+			return true;
+		}
+		return false;
+	}
+	public int getSize(){
+		return tour.size();
+	}
+	
+	public int getFitness() {
+		if(fitness == 0){
+			calcFitness();
+		}
+		return this.fitness;
+	}
+
+	private void calcFitness() {
+		if(fitness == 0){
+			for (int i = 0; i < tour.size(); i++) {
+				if(i != tour.size()-1){
+					fitness += tour.get(i).distanceTo(tour.get(i+1));
+				}
+				else {
+					fitness += tour.get(i).distanceTo(tour.get(0));
+				}
+			}
+		}
+	}
+	
+	public static Tour genTour() {
+		ArrayList<City> newTourList = CityHandler.getCities();
+		Collections.shuffle(newTourList);
+		Tour newTour = new Tour(newTourList);
+		return newTour;
+	}
 	
 }
